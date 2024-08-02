87c8ec1bae22241cd2f15c01d9dab8476622a894
Brenda Ngugi <brendangugi7@gmail.com>
2024-08-02 14:24:41 -0700

    correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" A func to calculate the perimeter of a grid
-    0 represents water
-    1 represents land
-    Each cell is square, with a side length of 1
-"""
+"""Island perimeter computing module."""
 
 
 def island_perimeter(grid):
-    """ Find perimeter from a list """
-    island_size = sum([i for list in grid for i in list])
-    perimeter = island_size * 2 + 2
-
-    # correct for inner tiles
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[i]) - 1):
-            if (grid[i][j] == 1 and grid[i+1][j] == 1
-                    and grid[i][j+1] == 1 and grid[i+1][j+1] == 1):
-                perimeter -= 2
-
+    """Compute the perimeter of an island with no lakes."""
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
